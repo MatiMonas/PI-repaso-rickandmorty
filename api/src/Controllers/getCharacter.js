a247171a1ea4056698203aebc93a87eec335c724
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2021-11-01 21:42:50 -0300

    getById refactor

--- api/src/Controllers/getCharacter.js
+++ api/src/Controllers/getCharacter.js
@@ -1,27 +1,62 @@
 const axios = require('axios');
 const { Character, Episode } = require('../Models/index');
 
-
 //BUSQUEDA POR IDS
 const getCharacter = async (req, res, next) => {
-    const { id } = req.params;
-    const isFromDb = id.includes('-');
-    let character;
-    try {
-        if (!id) return next({ msg: 'No character id sent. FATAL ERROR' });
-        console.log('entre')
+  const { id } = req.params;
+  const isFromDb = id.includes('-');
+  let character;
+  try {
+    if (!id) return next({ msg: 'No character id sent. FATAL ERROR' });
 
-        if (isFromDb) {
-            character = await Character.findByPk(id, {
-                include: Episode,
-            });
-            return res.json(character);
-        } else {
-            character = await axios.get('https://rickandmortyapi.com/api/character/' + id);
-            return res.json(character.data);
-        }
-    } catch (err) {
-        next(err);
-    } 
+    if (isFromDb) {
+      character = await Character.findByPk(id, {
+        include: Episode,
+      });
+
+      character = {
+        id: character.id,
+        name: character.name,
+        image: character.image,
+        episodes: character.Episodes.map((episode) => {
+          return {
+            id: episode.id,
+            name: episode.name,
+          };
+        }),
+      };
+    } else {
+      let characterResponse = await axios.get(
+        'https://rickandmortyapi.com/api/character/' + id,
+      );
+      characterResponse = characterResponse.data;
+
+      let episodesList = characterResponse.episode.map((episode) =>
+        episode.split('/').pop(),
+      );
+
+      episodesList = episodesList.join(',');
+
+      let episodes = await axios.get(
+        'https://rickandmortyapi.com/api/episode/' + episodesList,
+      );
+      episodes = episodes.data.map((episodes) => {
+        return {
+          id: episodes.id,
+          name: episodes.name,
+        };
+      });
+
+      character = {
+        id: characterResponse.id,
+        name: characterResponse.name,
+        image: characterResponse.image,
+        episodes: episodes,
+      };
+    }
+  } catch (err) {
+    next(err);
+  }
+  res.json(character);
 };
 module.exports = getCharacter;
