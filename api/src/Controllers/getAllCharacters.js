de09351ba4bef14e3dc76883c055a4957175df94
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2021-11-01 22:26:52 -0300

    eto eta roto

--- api/src/Controllers/getAllCharacters.js
+++ api/src/Controllers/getAllCharacters.js
@@ -3,48 +3,57 @@ const axios = require('axios');
 const { Op } = require('sequelize');
 
 const getAllCharacters = async (req, res, next) => {
-    const { name } = req.query;
-    let DBcharacters;
-    let APIcharacters;
+  const { name } = req.query;
+  let dataBaseCharacters;
+  let APIcharacters;
 
-    if (name) {
-        try {
-            
-            DBcharacters = await Character.findAll({
-                where: {
-                    name: {
-                        [Op.like]: `%${name}%`,
-                    },
-                },
-            });
+  if (name) {
+    try {
+      dataBaseCharacters = await Character.findAll({
+        where: {
+          name: {
+            [Op.iLike]: `%${name}%`,
+          },
+        },
+      });
 
-            let response = await axios.get(
-                'https://rickandmortyapi.com/api/character/?name=' + name,
-            );
-            APIcharacters = response.data.results;
-        } catch (err) {
-            next(err);
-        }
-        const characters = [...DBcharacters, ...APIcharacters];
-        res.json(characters);
-    } else {
-        try {
-            const API_characters = await axios.get('https://rickandmortyapi.com/api/character/');
-            const API_results = API_characters.data.results;
-            const DB_characters = await Character.findAll();
+      let characterResult = await axios.get(
+        `https://rickandmortyapi.com/api/character/?name=${name}`,
+      );
+      APIcharacters = characterResult.data.results?.map((character) => {
+        return {
+          id: character.id,
+          name: character.name,
+          image: character.image,
+        };
+      });
 
-            const API_formated_characters = API_results.map((character) => {
-                return {
-                    id: character.id,
-                    name: character.name,
-                    image: character.image,
-                };
-            });
-            res.json([...DB_characters, ...API_formated_characters]);
-        } catch (err) {
-            next(err);
-        }
+      const characters = [...dataBaseCharacters, ...APIcharacters];
+
+      res.send('hola');
+    } catch (error) {
+      next(error);
     }
+  } else {
+    try {
+      const API_characters = await axios.get(
+        'https://rickandmortyapi.com/api/character/',
+      );
+      const API_results = API_characters.data.results;
+      const DB_characters = await Character.findAll();
+
+      const API_formated_characters = API_results.map((character) => {
+        return {
+          id: character.id,
+          name: character.name,
+          image: character.image,
+        };
+      });
+      res.json([...DB_characters, ...API_formated_characters]);
+    } catch (err) {
+      console.log('rompi');
+    }
+  }
 };
 
 module.exports = getAllCharacters;
